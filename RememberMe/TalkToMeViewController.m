2b452b032a86dbc93d0b0bf24c5b9778aede1db4
Angela Gigliotti <angelagigliotti@Angelas-MBP.home>
2015-04-16 20:29:15 -0400

    Added Offline Speech Recognition

--- RememberMe/TalkToMeViewController.m
+++ RememberMe/TalkToMeViewController.m
@@ -14,17 +14,25 @@
 #import "TalkToMeViewController.h"
 #import <OpenEars/OEPocketsphinxController.h>
 #import <OpenEars/OEAcousticModel.h>
+#import <OpenEars/OELanguageModelGenerator.h>
+#import <OpenEars/OEAcousticModel.h>
+#import <OpenEars/OEEventsObserver.h>
 
 @implementation TalkToMeViewController
+@end
+@interface ViewController : UIViewController <OEEventsObserverDelegate>
+@property (strong, nonatomic) OEEventsObserver *openEarsEventsObserver;
 
 
--(void)viewDidLoad {
+-(void)viewDidLoad; {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
     self.fliteController = [[OEFliteController alloc] init]; //...
     self.slt = [[Slt alloc] init]; //...
     [self.fliteController say:@ "Hello, talk to me." withVoice:self.slt];
+    self.openEarsEventsObserver = [[OEEventsObserver alloc] init];
+    [self.openEarsEventsObserver setDelegate:self];
     self.openEarsEventsObserver = [[OEEventsObserver alloc] init];
     [self.openEarsEventsObserver setDelegate:self];
 }
@@ -66,8 +74,76 @@
     NSLog(@"A test file that was submitted for recognition is now complete.");
 }
 
-//create your language model
+//create your language model. Enter your words and phrases in all capital letters, since the model is generated against a dictionary in which the entries are capitalized
 
+OELanguageModelGenerator *lmGenerator = [[OELanguageModelGenerator alloc] init];
+
+NSArray *words = [NSArray arrayWithObjects:@"WORD", @"STATEMENT", @"OTHER WORD", @"A PHRASE", nil];
+NSString *name = @"NameIWantForMyLanguageModelFiles";
+NSError *err = [lmGenerator generateLanguageModelFromArray:words withFilesNamed:name forAcousticModelAtPath:[OEAcousticModel pathToModel:@"AcousticModelEnglish"]]; // Change "AcousticModelEnglish" to "AcousticModelSpanish" to create a Spanish language model instead of an English one.
+
+NSString *lmPath = nil;
+NSString *dicPath = nil;
+
+if(err == nil) {
+    
+    lmPath = [lmGenerator pathToSuccessfullyGeneratedLanguageModelWithRequestedName:@"NameIWantForMyLanguageModelFiles"];
+    dicPath = [lmGenerator pathToSuccessfullyGeneratedDictionaryWithRequestedName:@"NameIWantForMyLanguageModelFiles"];
+    
+} else {
+    NSLog(@"Error: %@",[err localizedDescription]);
+}
+
+
+//method where you want to recognize speech
+
+[[OEPocketsphinxController sharedInstance] setActive:TRUE error:nil];
+[[OEPocketsphinxController sharedInstance] startListeningWithLanguageModelAtPath:lmPath dictionaryAtPath:dicPath acousticModelAtPath:[OEAcousticModel pathToModel:@"AcousticModelEnglish"] languageModelIsJSGF:NO]; // Change "AcousticModelEnglish" to "AcousticModelSpanish" to perform Spanish recognition instead of English
+
+//OEEventsObserver to your class, which is where you will receive information about received speech hypotheses and other speech UI events:
+- (void) pocketsphinxDidReceiveHypothesis:(NSString *)hypothesis recognitionScore:(NSString *)recognitionScore utteranceID:(NSString *)utteranceID {
+    NSLog(@"The received hypothesis is %@ with a score of %@ and an ID of %@", hypothesis, recognitionScore, utteranceID);
+}
+
+- (void) pocketsphinxDidStartListening {
+    NSLog(@"Pocketsphinx is now listening.");
+}
+
+- (void) pocketsphinxDidDetectSpeech {
+    NSLog(@"Pocketsphinx has detected speech.");
+}
+
+- (void) pocketsphinxDidDetectFinishedSpeech {
+    NSLog(@"Pocketsphinx has detected a period of silence, concluding an utterance.");
+}
+
+- (void) pocketsphinxDidStopListening {
+    NSLog(@"Pocketsphinx has stopped listening.");
+}
+
+- (void) pocketsphinxDidSuspendRecognition {
+    NSLog(@"Pocketsphinx has suspended recognition.");
+}
+
+- (void) pocketsphinxDidResumeRecognition {
+    NSLog(@"Pocketsphinx has resumed recognition.");
+}
+
+- (void) pocketsphinxDidChangeLanguageModelToFile:(NSString *)newLanguageModelPathAsString andDictionary:(NSString *)newDictionaryPathAsString {
+    NSLog(@"Pocketsphinx is now using the following language model: \n%@ and the following dictionary: %@",newLanguageModelPathAsString,newDictionaryPathAsString);
+}
+
+- (void) pocketSphinxContinuousSetupDidFailWithReason:(NSString *)reasonForFailure {
+    NSLog(@"Listening setup wasn't successful and returned the failure reason: %@", reasonForFailure);
+}
+
+- (void) pocketSphinxContinuousTeardownDidFailWithReason:(NSString *)reasonForFailure {
+    NSLog(@"Listening teardown wasn't successful and returned the failure reason: %@", reasonForFailure);
+}
+
+- (void) testRecognitionCompleted {
+    NSLog(@"A test file that was submitted for recognition is now complete.");
+}
 
 
 @end
