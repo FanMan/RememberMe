0b67ea1fe9b192c62b9bc5809e91e0c75d394294
Angela Gigliotti <angelagigliotti@Angelas-MBP.home>
2015-04-16 20:42:45 -0400

    removed duplicates

--- RememberMe/TalkToMeViewController.m
+++ RememberMe/TalkToMeViewController.m
@@ -57,8 +57,8 @@
 
 //add these delegate methods of OEEventsObserver to your class, which is where you will receive information about received speech hypothesis and other speech UI events:
 
-- (void)pocketsphnixDidReceiveHypothesis:(NSString *)hypothesis recognitionScore:(NSString *)recognitionScore utteranceID:(NSString *)utteranceID;{
-    NSLog(@"The received hypothesis is %@ with a score of %a and an ID of %a", hypothesis, recognitionScore, utteranceID);
+- (void)pocketsphnixDidReceiveHypothesis:(NSString *)hypothesis recognitionScore:(NSString *)recognitionScore utteranceID:(NSString *)utteranceID{
+    NSLog(@"The received hypothesis is %@ with a score of %a and an ID of %a", hypothesis, recognitionScore, utteranceID)
 }
 
 - (void)pocketsphinxidStartListening {
@@ -99,52 +99,6 @@
 
 [[OEPocketsphinxController sharedInstance] setActive:TRUE error:nil];
 [[OEPocketsphinxController sharedInstance] startListeningWithLanguageModelAtPath:lmPath dictionaryAtPath:dicPath acousticModelAtPath:[OEAcousticModel pathToModel:@"AcousticModelEnglish"] languageModelIsJSGF:NO]; // Change "AcousticModelEnglish" to "AcousticModelSpanish" to perform Spanish recognition instead of English
-
-//OEEventsObserver to your class, which is where you will receive information about received speech hypotheses and other speech UI events:
-    
-- (void) pocketsphinxDidReceiveHypothesis:(NSString *)hypothesis recognitionScore:(NSString *)recognitionScore utteranceID:(NSString *)utteranceID {
-    NSLog(@"The received hypothesis is %@ with a score of %@ and an ID of %@", hypothesis, recognitionScore, utteranceID);
-}
-
-- (void) pocketsphinxDidStartListening {
-    NSLog(@"Pocketsphinx is now listening.");
-}
-
-- (void) pocketsphinxDidDetectSpeech {
-    NSLog(@"Pocketsphinx has detected speech.");
-}
-
-- (void) pocketsphinxDidDetectFinishedSpeech {
-    NSLog(@"Pocketsphinx has detected a period of silence, concluding an utterance.");
-}
-
-- (void) pocketsphinxDidStopListening {
-    NSLog(@"Pocketsphinx has stopped listening.");
-}
-
-- (void) pocketsphinxDidSuspendRecognition {
-    NSLog(@"Pocketsphinx has suspended recognition.");
-}
-
-- (void) pocketsphinxDidResumeRecognition {
-    NSLog(@"Pocketsphinx has resumed recognition.");
-}
-
-- (void) pocketsphinxDidChangeLanguageModelToFile:(NSString *)newLanguageModelPathAsString andDictionary:(NSString *)newDictionaryPathAsString {
-    NSLog(@"Pocketsphinx is now using the following language model: \n%@ and the following dictionary: %@",newLanguageModelPathAsString,newDictionaryPathAsString);
-}
-
-- (void) pocketSphinxContinuousSetupDidFailWithReason:(NSString *)reasonForFailure {
-    NSLog(@"Listening setup wasn't successful and returned the failure reason: %@", reasonForFailure);
-}
-
-- (void) pocketSphinxContinuousTeardownDidFailWithReason:(NSString *)reasonForFailure {
-    NSLog(@"Listening teardown wasn't successful and returned the failure reason: %@", reasonForFailure);
-}
-
-- (void) testRecognitionCompleted {
-    NSLog(@"A test file that was submitted for recognition is now complete.");
-}
 
 
 @end
